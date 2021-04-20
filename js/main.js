d52f8e1b366495643cfc380478fac84408c01bfe
”LynxTi” <andreytimoshenko92@gmail.com>
2021-04-20 17:21:04 +0300

    hw calc

---
+++ js/main.js
@@ -0,0 +1,43 @@
+"use strict"
+
+const btnContainer = document.querySelector('.buttons');
+const rezaltBlock = document.querySelector('.rezalt-block');
+
+btnContainer.addEventListener('click',(event) => {
+    if (event.target !== btnContainer) {
+        const firstNum = Number(document.querySelector('.firstNum').value);
+        const secondNum = Number(document.querySelector('.secondNum').value);
+        const numberAfterComa = Number(document.querySelector('.numberAfterComa').value);
+        const btn = event.target;
+        const mathSymbol = btn.dataset.simbol;
+    
+        const rezalt = mathOperation(firstNum, secondNum, mathSymbol);
+    
+        showRezalt(rezalt,numberAfterComa); 
+    }
+})
+
+const mathOperation = (firstNum, secondNum, symbol) => {
+    switch (symbol) {
+        case 'plus':
+            return firstNum + secondNum;
+        case 'minus':
+            return firstNum - secondNum;
+        case 'multiply':
+            return firstNum * secondNum;
+        case 'divide':
+            return firstNum / secondNum;
+        case 'squaring':
+            return Math.pow(firstNum, secondNum);
+        // case 'square':
+        //     return
+    }
+}
+
+const showRezalt = (rezalt, numberAfterComa)=>{
+    if (numberAfterComa === 0) {
+        numberAfterComa = 10;
+    }
+
+    rezaltBlock.innerHTML = rezalt.toFixed(numberAfterComa)
+}
