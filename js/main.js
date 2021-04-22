b67f2d6fb8cb1fe2dfc574f4a862da963e8fff88
”LynxTi” <andreytimoshenko92@gmail.com>
2021-04-22 21:10:15 +0300

    formData

--- js/main.js
+++ js/main.js
@@ -2,18 +2,25 @@
 
 const btnContainer = document.querySelector('.buttons');
 const rezaltBlock = document.querySelector('.rezalt-block');
+const form = document.querySelector('.form');
+
 
 btnContainer.addEventListener('click',(event) => {
     if (event.target !== btnContainer) {
-        const firstNum = Number(document.querySelector('.firstNum').value);
-        const secondNum = Number(document.querySelector('.secondNum').value);
-        const numberAfterComa = Number(document.querySelector('.numberAfterComa').value);
+        // const firstNum = Number(document.querySelector('.firstNum').value);
+        // const secondNum = Number(document.querySelector('.secondNum').value);
+        // const numberAfterComa = Number(document.querySelector('.numberAfterComa').value);
+        const valueIputs = getValue();
+        const firstNum = valueIputs[0];
+        const secondNum = valueIputs[1];
+        const numberAfterComa = valueIputs[2];
+
         const btn = event.target;
         const mathSymbol = btn.dataset.simbol;
-    
         const rezalt = mathOperation(firstNum, secondNum, mathSymbol);
     
         showRezalt(rezalt,numberAfterComa); 
+        
     }
 })
 
@@ -40,4 +47,14 @@ const showRezalt = (rezalt, numberAfterComa)=>{
     }
 
     rezaltBlock.innerHTML = rezalt.toFixed(numberAfterComa)
+}
+
+const getValue = () => {  //Новый метод, добавил последним 
+    const formData = new FormData(form);
+    let value= [];
+    value.push(Number(formData.get('firstNum')));
+    value.push(Number(formData.get('secondNum')));
+    value.push(Number(formData.get('numberAfterComa')));
+
+    return value;    
 }
